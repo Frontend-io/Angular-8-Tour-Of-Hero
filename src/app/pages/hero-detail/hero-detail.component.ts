b867420c3328cfbd82cd72984fdd9e6cbfb2de3d
Iyobosa Jefferson Osagie <iyobosajefferson2@gmail.com>
2019-11-11 08:00:29 +0100

    Completed the static phase of Tour of Hero project

--- src/app/pages/hero-detail/hero-detail.component.ts
+++ src/app/pages/hero-detail/hero-detail.component.ts
@@ -8,7 +8,7 @@ import { Hero } from "../../hero";
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() public hero: Hero;;;;;;;;;
+  @Input() public hero: Hero;
 
   
   constructor() { }
